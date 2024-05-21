e4c228d19b12e5ded8c8a0fedce859fdbd98e7a5
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-22 01:10:12 +0300

    BaseModel Exercise

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+class BaseModel acts as a base model for other classes
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        """Constructor: initializes id, created_at & updated_at"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """updates updated_at with the current datetime"""
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """Returns a dict -> all keys/values of __dict__ instance"""
+        instanceDict = self.__dict__.copy()
+        instanceDict["__class__"] = self.__class__.__name__
+        instanceDict["created_at"] = self.created_at.isoformat()
+        instanceDict["updated_at"] = self.updated_at.isoformat()
+        return instanceDict
+
+    def __str__(self):
+        """Prints [<class name>] (<self.id>) <self.__dict__>"""
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
---
+++ tests/test_model/test_base_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Tests for base_model.py --> BaseModel class
+"""
+from models.base_model import BaseModel
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_init(self):
+        """
+        Testing the constructor
+        """
+        my_model = BaseModel()
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def test_save(self):
+        """
+        Testing the save method
+        """
+        my_model = BaseModel()
+        init_updated_at = my_model.updated_at
+        curr_updated_at = my_model.save()
+        self.assertNotEqual(init_updated_at, curr_updated_at)
+
+    def test_to_dict(self):
+        """
+        Testing to_dict method
+        """
+        model = BaseModel()
+        model_dict = model.to_dict
+        message = "Object is not an instance of..."
+        # self.assertIsInstance(model_dict, dict, message)
+        self.assertEqual(model_dict()["__class__"], 'BaseModel')
+        self.assertEqual(model_dict()['id'], model.id)
+        self.assertEqual(model_dict()['created_at'], model.created_at.isoformat())
+        self.assertEqual(model_dict()['updated_at'], model.updated_at.isoformat())
+
+    def test_str_method(self):
+        """
+        Testing the str method
+        """
+        model = BaseModel()
+        self.assertTrue(str(model).startswith('[BaseModel]'))
+        self.assertIn(model.id, str(model))
+        self.assertIn(str(model.__dict__), str(model))
+
+if __name__ == "__main__":
+    unittest.main()
