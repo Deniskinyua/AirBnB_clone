d9363b05ec328d655afb1d3acb90337a41bb1c99
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-21 14:38:01 +0300

    Pycodestyle Compliant

---
+++ Compliant.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A python class"""
+
+
+class Student:
+
+    def __init__(self, first, last, studentIndex):
+        self.first = first
+        self.last = last
+        self.studentIndex = studentIndex
+
+    def fullname(self):
+        return '{} {}'.format.self.first, self.last)
