ce4a34204531061fa06a2948f6ba00cd538dee96
Denis Kinyua <kimenjudenis@gmail.com>
2024-05-21 14:47:42 +0300

    Pycodestyle Compliant

--- Compliant.py
+++ Compliant.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-""" A python class"""
 
-
-class Student:
-
-    def __init__(self, first, last, studentIndex):
-        self.first = first
-        self.last = last
-        self.studentIndex = studentIndex
-
-    def fullname(self):
-        return '{} {}'.format.self.first, self.last)
+print("The AirBnB Clone Project")
